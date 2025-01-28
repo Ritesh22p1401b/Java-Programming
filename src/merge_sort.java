b3738306338394ce34f3103fcbaa697601cc9e65
Ritesh Pandey <ritesh2897pandey@gmail.com>
2025-01-28 15:24:03 +0530

    merge_sort

---
+++ src/merge_sort.java
@@ -0,0 +1,15 @@
+import java.sql.SQLOutput;
+import java.util.*;
+import java.io.*;
+
+
+public class merge_sort {
+    public static void main(String[] args){
+        int[] arr = {5,4,3,2,1};
+        sorting(arr,0,arr.length);
+        System.out.println(Arrays.toString(arr));
+    }
+    
+
+
+}
